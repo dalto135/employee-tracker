62cc32e03b9dcc5c25d90aab2ef0f3df4a4d7b5a
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-04-12 15:19:03 -0400

    created sql file, downloaded console.table

---
+++ employee-tracker.sql
@@ -0,0 +1,22 @@
+create database employee_trackerdb;
+use employee_trackerdb;
+create table department (
+	id int not null,
+	name varchar(30),
+    primary key (id)
+);
+create table role (
+	id int not null,
+	title varchar(30),
+    salary decimal,
+    department_id int,
+	primary key (id)
+);
+create table employee (
+	id int not null,
+	first_name varchar(30),
+    last_name varchar(30),
+    role_id int,
+    manager_id int,
+    primary key (id)
+);
